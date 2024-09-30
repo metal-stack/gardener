c0684f1fb4b7bd4a80c4d7f68ce727a89deb991d
Gerrit <tirreg91@googlemail.com>
2024-09-30 11:51:48 +0200

    Crash 1.89 with K8s 1.30

--- pkg/utils/validation/kubernetesversion/version.go
+++ pkg/utils/validation/kubernetesversion/version.go
@@ -29,6 +29,7 @@ var SupportedVersions = []string{
 	"1.27",
 	"1.28",
 	"1.29",
+	"1.30",
 }
 
 // CheckIfSupported checks if the provided version is part of the supported Kubernetes versions list.

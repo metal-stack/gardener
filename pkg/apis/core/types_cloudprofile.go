6da67e4d696194704420f33d1b265c89d6651f03
Luca Bernstein <luca.bernstein@sap.com>
2025-06-05 08:57:40 +0200

    Adapt types for version classification lifecycles
    Co-authored-by: Valentin Knabel <dev@vknabel.com>
    Co-authored-by: Gerrit <Gerrit91@users.noreply.github.com>
    Co-authored-by: Claus-Theodor Riegg <crigertg@users.noreply.github.com>
    Co-authored-by: Luca Bernstein <LucaBernstein@users.noreply.github.com>
    Signed-off-by: Luca Bernstein <luca.bernstein@sap.com>

--- pkg/apis/core/types_cloudprofile.go
+++ pkg/apis/core/types_cloudprofile.go
@@ -26,6 +26,8 @@ type CloudProfile struct {
 	metav1.ObjectMeta
 	// Spec defines the provider environment properties.
 	Spec CloudProfileSpec
+	// Status contains the current status of the cloud profile.
+	Status CloudProfileStatus
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
@@ -143,9 +145,20 @@ type ExpirableVersion struct {
 	// Version is the version identifier.
 	Version string
 	// ExpirationDate defines the time at which this version expires.
+	// Deprecated: Is replaced by Lifecycle.
 	ExpirationDate *metav1.Time
-	// Classification defines the state of a version (preview, supported, deprecated)
+	// Classification defines the state of a version (preview, supported, deprecated).
+	// Deprecated: Is replaced by Lifecycle.
 	Classification *VersionClassification
+	// Lifecycle defines the lifecycle stages for this version. Cannot be used in combination with deprecated Classification and ExpirationDate.
+	Lifecycle []LifecycleStage
+}
+
+type LifecycleStage struct {
+	// Classification is the category of this lifecycle stage (unavailable, preview, supported, deprecated, expired).
+	Classification VersionClassification
+	// StartTime defines when this lifecycle stage becomes active.
+	StartTime *metav1.Time
 }
 
 // MachineType contains certain properties of a machine type.
@@ -247,6 +260,29 @@ type BastionMachineType struct {
 	Name string
 }
 
+// CloudProfileStatus contains the status of the cloud profile.
+type CloudProfileStatus struct {
+	// KubernetesVersions contains the statuses of the kubernetes versions.
+	KubernetesVersions []ExpirableVersionStatus
+	// MachineImageVersions contains the statuses of the machine image versions.
+	MachineImageVersions []MachineImageVersionStatus
+}
+
+type MachineImageVersionStatus struct {
+	// Name matches the name of the MachineImage the status is represented of.
+	Name string
+	// Versions contains the statuses of the machine image versions.
+	Versions []ExpirableVersionStatus
+}
+
+// ExpirableVersionStatus defines the current status of an expirable version.
+type ExpirableVersionStatus struct {
+	// Version is the version identifier.
+	Version string
+	// Classification reflects the current state in the classification lifecycle.
+	Classification VersionClassification
+}
+
 // Limits configures operational limits for Shoot clusters using this CloudProfile.
 // See https://github.com/gardener/gardener/blob/master/docs/usage/shoot/shoot_limits.md.
 type Limits struct {
@@ -264,7 +300,21 @@ const (
 // VersionClassification is the logical state of a version.
 type VersionClassification string
 
+var order = map[VersionClassification]int{
+	ClassificationUnavailable: 0,
+	ClassificationPreview:     1,
+	ClassificationSupported:   2,
+	ClassificationDeprecated:  3,
+	ClassificationExpired:     4,
+}
+
+func (c1 VersionClassification) Compare(c2 VersionClassification) int {
+	return order[c1] - order[c2]
+}
+
 const (
+	// ClassificationUnavailable indicates that a version is currently not available and is planned to become available depending on the classification lifecycle.
+	ClassificationUnavailable VersionClassification = "unavailable"
 	// ClassificationPreview indicates that a version has recently been added and not promoted to "Supported" yet.
 	// ClassificationPreview versions will not be considered for automatic Kubernetes and Machine Image patch version updates.
 	ClassificationPreview VersionClassification = "preview"
@@ -275,6 +325,9 @@ const (
 	// ClassificationDeprecated indicates that a patch version should not be used anymore, should be updated to a new version
 	// and will eventually expire.
 	ClassificationDeprecated VersionClassification = "deprecated"
+	// ClassificationExpired indicates that a version has expired.
+	// New entities with that version cannot be created and existing entities are forcefully migrated to a higher version during the maintenance time.
+	ClassificationExpired VersionClassification = "expired"
 )
 
 // MachineImageUpdateStrategy is the update strategy to use for a machine image
--- pkg/apis/core/v1beta1/types_cloudprofile.go
+++ pkg/apis/core/v1beta1/types_cloudprofile.go
@@ -29,6 +29,8 @@ type CloudProfile struct {
 	// Spec defines the provider environment properties.
 	// +optional
 	Spec CloudProfileSpec `json:"spec,omitempty" protobuf:"bytes,2,opt,name=spec"`
+	// Status contains the current status of the cloud profile.
+	Status CloudProfileStatus `json:"status,omitempty" protobuf:"bytes,3,opt,name=status"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
@@ -160,11 +162,24 @@ type ExpirableVersion struct {
 	// Version is the version identifier.
 	Version string `json:"version" protobuf:"bytes,1,opt,name=version"`
 	// ExpirationDate defines the time at which this version expires.
+	// Deprecated: Is replaced by Lifecycle.
 	// +optional
 	ExpirationDate *metav1.Time `json:"expirationDate,omitempty" protobuf:"bytes,2,opt,name=expirationDate"`
-	// Classification defines the state of a version (preview, supported, deprecated)
+	// Classification defines the state of a version (preview, supported, deprecated).
+	// Deprecated: Is replaced by Lifecycle.
 	// +optional
 	Classification *VersionClassification `json:"classification,omitempty" protobuf:"bytes,3,opt,name=classification,casttype=VersionClassification"`
+	// Lifecycle defines the lifecycle stages for this version. Cannot be used in combination with Classification and ExpirationDate.
+	// +optional
+	Lifecycle []LifecycleStage `json:"lifecycle,omitempty" protobuf:"bytes,4,opt,name=lifecycle"`
+}
+
+type LifecycleStage struct {
+	// Classification is the category of this lifecycle stage (unavailable, preview, supported, deprecated, expired).
+	Classification VersionClassification `json:"classification" protobuf:"bytes,1,opt,name=classification,casttype=VersionClassification"`
+	// StartTime defines when this lifecycle stage becomes active.
+	// +optional
+	StartTime *metav1.Time `json:"startTime,omitempty" protobuf:"bytes,2,opt,name=startTime"`
 }
 
 // MachineType contains certain properties of a machine type.
@@ -292,6 +307,29 @@ type Limits struct {
 	MaxNodesTotal *int32 `json:"maxNodesTotal,omitempty" protobuf:"varint,1,opt,name=maxNodesTotal"`
 }
 
+// CloudProfileStatus contains the status of the cloud profile.
+type CloudProfileStatus struct {
+	// KubernetesVersions contains the statuses of the kubernetes versions.
+	KubernetesVersions []ExpirableVersionStatus `json:"kubernetesVersions,omitempty" protobuf:"bytes,1,name=kubernetesVersions"`
+	// MachineImageVersions contains the statuses of the machine image versions.
+	MachineImageVersions []MachineImageVersionStatus `json:"machineImageVersions,omitempty" protobuf:"bytes,2,name=machineImageVersions"`
+}
+
+type MachineImageVersionStatus struct {
+	// Name matches the name of the MachineImage the status is represented of.
+	Name string `json:"name,omitempty" protobuf:"bytes,1,name=name"`
+	// Versions contains the statuses of the machine image versions.
+	Versions []ExpirableVersionStatus `json:"versions,omitempty" protobuf:"bytes,2,name=versions"`
+}
+
+// ExpirableVersionStatus defines the current status of an expirable version.
+type ExpirableVersionStatus struct {
+	// Version is the version identifier.
+	Version string `json:"version" protobuf:"bytes,1,opt,name=version"`
+	// Classification reflects the current state in the classification lifecycle.
+	Classification VersionClassification `json:"classification,omitempty" protobuf:"bytes,2,opt,name=classification,casttype=VersionClassification"`
+}
+
 const (
 	// VolumeClassStandard is a constant for the standard volume class.
 	VolumeClassStandard string = "standard"
@@ -303,6 +341,8 @@ const (
 type VersionClassification string
 
 const (
+	// ClassificationUnavailable indicates that a version is currently not available and is planned to become available depending on the classification lifecycle.
+	ClassificationUnavailable VersionClassification = "unavailable"
 	// ClassificationPreview indicates that a version has recently been added and not promoted to "Supported" yet.
 	// ClassificationPreview versions will not be considered for automatic Kubernetes and Machine Image patch version updates.
 	ClassificationPreview VersionClassification = "preview"
@@ -313,6 +353,9 @@ const (
 	// ClassificationDeprecated indicates that a patch version should not be used anymore, should be updated to a new version
 	// and will eventually expire.
 	ClassificationDeprecated VersionClassification = "deprecated"
+	// ClassificationExpired indicates that a version has expired.
+	// New entities with that version cannot be created and existing entities are forcefully migrated to a higher version during the maintenance time.
+	ClassificationExpired VersionClassification = "expired"
 )
 
 // MachineImageUpdateStrategy is the update strategy to use for a machine image

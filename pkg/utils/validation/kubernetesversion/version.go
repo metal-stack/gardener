20394c87cb4dbcb047159c220dde2280e58fdf1f
Stefan Majer <stefan.majer@f-i-ts.de>
2025-10-17 12:23:07 +0200

    Do not crash with kubernetes 1.33

--- pkg/utils/validation/kubernetesversion/version.go
+++ pkg/utils/validation/kubernetesversion/version.go
@@ -19,6 +19,8 @@ var SupportedVersions = []string{
 	"1.30",
 	"1.31",
 	"1.32",
+	"1.33",
+	"1.34",
 }
 
 // CheckIfSupported checks if the provided version is part of the supported Kubernetes versions list.
